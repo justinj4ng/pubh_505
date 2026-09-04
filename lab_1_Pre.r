393a56b399ef7c5b7a55fb9ade350de8e9dd96de
justinj4ng <2255justin@gmail.com>
2026-09-04 13:56:02 -0400

    na

--- lab_1_Pre.r
+++ lab_1_Pre.r
@@ -2,10 +2,8 @@
 rm(list=ls())
 graphics.off()
 
-#setwd("C:/Users/Justin Jang/Documents/pubh_505_biostatistics_in_public_health/lab_1")
+setwd("~/pubh_505")
 
-
-#setwd("~/pubh_505_biostatistics_in_public_health")
 nyc <- read.csv("NYC_poverty.csv")
 
 
