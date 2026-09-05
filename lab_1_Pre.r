b9a8c43667b0f294b28d52611d466913f8dfca2d
justinj4ng <2255justin@gmail.com>
2026-09-05 02:01:59 -0400

    #1, #2 start

--- lab_1_Pre.r
+++ lab_1_Pre.r
@@ -1,4 +1,8 @@
 #Lab 01
+
+
+# iffelse("column<>=","trait 1", "trait 2" ) 
+
 rm(list=ls())
 graphics.off()
 
@@ -6,10 +10,19 @@ setwd("~/pubh_505")
 
 nyc <- read.csv("NYC_poverty.csv")
 
+# 1. nrows(nyc) is 26680
 
+# eliminate all columns except these 3
+nyc <- nyc[ , c("SEX", "Boro", "PreTaxIncome_PU")]
+
+colnames(nyc)[1] <- "sex"
+colnames(nyc)[2] <- "boro"
+colnames(nyc)[3] <- "income"
+
+
+# 2. ncol(nyc) is 3
+
+# replace numerical designation of boros with name
+nyc[nyc$boro == ""] <- 
 nyc
-
-
-
-#iffelse("column<>=","trait 1", "trait 2" ) 
-
+nyc
