559d8bc54ee549a002a0a8e37343b597eaa39936
justinj4ng <2255justin@gmail.com>
2026-09-06 02:00:33 -0400

    finished to end of #4

--- lab_1_Pre.r
+++ lab_1_Pre.r
@@ -23,6 +23,72 @@ colnames(nyc)[3] <- "income"
 # 2. ncol(nyc) is 3
 
 # replace numerical designation of boros with name
-nyc[nyc$boro == ""] <- 
-nyc
-nyc
+#vector subsetting
+nyc$boro[nyc$boro == "1"] <- "bronx"
+nyc$boro[nyc$boro == "2"] <- "brooklyn"
+nyc$boro[nyc$boro == "3"] <- "manhattan"
+nyc$boro[nyc$boro == "4"] <- "queens"
+nyc$boro[nyc$boro == "5"] <- "staten island"
+
+# make a table displaying the count how many times a boro appears in a column
+boro_table <-data.frame(
+  BORO = c("brooklyn", "bronx", "manhattan", "queens", "staten island"),
+  SUM = c(
+  sum(nyc$boro == "bronx"), # 3744
+  sum(nyc$boro == "brooklyn"), # 9268
+  sum(nyc$boro == "manhattan"), # 4430
+  sum(nyc$boro == "queens"), # 7779 
+  sum(nyc$boro == "staten island"))
+)# 1459
+
+
+print(boro_table)
+#BORO  SUM
+#1      brooklyn 3744
+#2         bronx 9268
+#3     manhattan 4430
+#4        queens 7779
+#5 staten island 1459
+
+
+# 4. 
+
+# replace numerical designation of sex with characters
+nyc$sex[nyc$sex == "1"] <- "male"
+nyc$sex[nyc$sex == "2"] <- "female"
+
+# make a table displaying count of how many males and females
+sex_table <- data.frame(
+  SEX = c("male", "female"),
+  SUM = c(
+  sum(nyc$sex == "male"),
+  sum(nyc$sex == "female")))
+  
+print(sex_table)  
+#SEX   SUM
+#1   male 11879
+#2 female 14801  
+
+
+# round income to nearest dollar
+nyc$income <- round(nyc$income, digits = 0)
+
+# make a new column to flag negative income values.
+nyc$debt <- ifelse(nyc$income > 0, "earning", "debt")
+
+# make a two-way table sex(table row), debt(table col)
+
+sex_debt_table <- table(nyc$sex, nyc$debt)
+
+print(sex_debt_table)
+# total number of observations in each sub-category
+
+#debt earning
+#female   315   14486
+#male     243   11636
+
+
+
+
+
+
