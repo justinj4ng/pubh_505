5eda028b033d231593c114c677abec6c79b3a93a
justinj4ng <2255justin@gmail.com>
2026-09-09 23:54:46 -0400

    lab_01 in submittable condition, review one more time

--- Lab_01_Section_06_JangJustin.r
+++ Lab_01_Section_06_JangJustin.r
@@ -1,18 +1,24 @@
-#Lab 01
+#Lab 01 
+#justin.jang@yale.edu
 
 
-# iffelse("column<>=","trait 1", "trait 2" ) 
+# 1.
 
 rm(list=ls())
 graphics.off()
 
 #install.packages("doBy")
+library(doBy)
 
 setwd("~/pubh_505")
 
 nyc <- read.csv("NYC_poverty.csv")
 
-# 1. nrows(nyc) is 26680
+
+nrow(nyc)
+# nrow(nyc) is 26680
+
+# 2.
 
 # eliminate all columns except these 3
 nyc <- nyc[ , c("SEX", "Boro", "PreTaxIncome_PU")]
@@ -22,9 +28,9 @@ colnames(nyc)[2] <- "boro"
 colnames(nyc)[3] <- "income"
 
 
-# 2. 
-ncol(nyc) 
-# 3
+print(paste("n variables =",ncol(nyc),"variables"))
+# n variables = 3 variables
+
 
 # 3.
 # replace numerical designation of boros with name
@@ -58,28 +64,28 @@ print(boro_table)
 
 # 4. 
 
-# replace numerical designation of sex with characters
-nyc$sex[nyc$sex == "1"] <- "male"
-nyc$sex[nyc$sex == "2"] <- "female"
+# change class of sex from integer to factor
+nyc$sex <- as.factor(nyc$sex)
+class(nyc$sex)
 
-# make a table displaying count of how many males and females
-sex_table <- data.frame(
-  SEX = c("male", "female"),
-  SUM = c(
-    sum(nyc$sex == "male"),
-    sum(nyc$sex == "female")))
-  
-print(sex_table)  
-#SEX   SUM
-#1   male 11879
-#2 female 14801  
+#change sex to male, female
 
+levels(nyc$sex) <- c("male", "female")
 
 # round income to nearest dollar
 nyc$income <- round(nyc$income, digits = 0)
 
+
+# make a table displaying count of how many males and females
+
+print(table(nyc$sex))  
+# male female 
+# 11879  14801 
+
+
+
 # make a new column to flag negative income values.
-nyc$debt <- ifelse(nyc$income > 0, "earning", "debt")
+nyc$debt <- ifelse(nyc$income >= 0, "earning", "owing")
 
 # make a two-way table sex(table row), debt(table col)
 
@@ -87,95 +93,53 @@ sex_debt_table <- table(nyc$sex, nyc$debt)
 
 print(sex_debt_table)
 # total number of observations in each sub-category
-
-#debt earning
-#female   315   14486
-#male     243   11636
+# earning owing
+# male     11873     6
+# female   14796     5
 
 # 5.
 # visualize frequency (number of persons) per each boro
 
-BORO = c("bronx", "brooklyn", "manhattan", "queens", "staten island")
-SUM = c(
-  sum(nyc$boro == "bronx"), # 3744
-  sum(nyc$boro == "brooklyn"), # 9268
-  sum(nyc$boro == "manhattan"), # 4430
-  sum(nyc$boro == "queens"), # 7779 
-  sum(nyc$boro == "staten island"))# 1459
+boro_prop <- prop.table(table(nyc$boro)) 
 
-boro_frq_plot <- barplot(height = SUM, names.arg = BORO, col = "tomato", ylab = "placeholder", ylim = c(0, 10000))
-# text()
+barplot(
+  boro_prop,
+  main = "Proportion of Dataset by NYC Borough",
+  xlab = "boro",
+  ylab = "relative frequency of observations"
+  )
 
 
 # 6.
 
 # visualize income by boro.show median income. 
+boxplot(nyc$income ~ nyc$boro, xlab = "boro", ylab = "income (USD)",
+        main = "income distribution by boro"
+        )
 
-# logical indexing, "Give me the income values where boro is "bronx"."
-median_income_by_boro <- c(
-  median(nyc$income[nyc$boro == "bronx"]),
-  median(nyc$income[nyc$boro == "brooklyn"]),
-  median(nyc$income[nyc$boro == "manhattan"]),
-  median(nyc$income[nyc$boro == "queens"]),
-  median(nyc$income[nyc$boro == "staten island"])
-)
-  
-median_income_by_boro
 
-# calc IQR
-IQR_income_by_boro <-c(
-IQR(nyc$income[nyc$boro == "bronx"]),
-IQR(nyc$income[nyc$boro == "brooklyn"]),
-IQR(nyc$income[nyc$boro == "manhattan"]),
-IQR(nyc$income[nyc$boro == "queens"]),
-IQR(nyc$income[nyc$boro == "staten island"])
-)
 
-# build error bars
-lower_bound <- median_income_by_boro - IQR_income_by_boro
-upper_bound <- median_income_by_boro + IQR_income_by_boro
 
-# bars<- boxplot(median_income_by_boro ~ BORO,
-#         data = nyc,
-#         main = "income by boro",
-#         xlab = "boro",
-#         ylab = "income")
-
-bars <- barplot(height = median_income_by_boro,
-                names.arg = BORO,
-                xlab = "boro",
-                ylim = c(0, max(upper_bound) + 2), 
-                main = "income by boro")
-bars
 
 # 7. calculate a representative value of the income within the  broader dataset
-summary(nyc$income)
-# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# -5542   25190   58440   90843  110835 3188008 
-# median
+#summary(nyc$income)
+print(paste("median:", median(nyc$income))) # .join equivalent, keeps space between strings
+print(paste("IQR:", IQR(nyc$income)))
 
-sd(nyc$income)
-#120089.8
+# "median: 58440"
+# "IQR: 85645"
+
 
 # 8. calculate median income 
-# median_income_by_boro
-# 37734.5 55452.5 73503.5 62470.0 78592.0
+boro_median <- summaryBy(income ~ boro, data = nyc, FUN = median) #FUN is Function. e.g., centrality or spread
 
-# https://www.rdocumentation.org/packages/doBy/versions/1.5/topics/summaryBy
-
-# income_boro <- data.frame(
-#   INCOME = 
-#     (nyc$income[nyc$boro == "bronx"]),
-#     (nyc$income[nyc$boro == "brooklyn"]),
-#     (nyc$income[nyc$boro == "manhattan"]),
-#     (nyc$income[nyc$boro == "queens"]),
-#     (nyc$income[nyc$boro == "staten island"]))
-#   
-#   BORO = c("bronx", "brooklyn", "manhattan", "queens", "staten island")
-# )
-
-# summary_data <- summaryBy(BORO ~ INCOME, data = income_boro, FUN = list(median))
-# print(summary_data)
+print(boro_median)
+# boro income.median
+# 1         bronx       37734.5
+# 2      brooklyn       55452.5
+# 3     manhattan       73503.5
+# 4        queens       62470.0
+# 5 staten island       78592.0
 
 
 
