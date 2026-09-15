728c164d80eb58ca0e80f190e0fcf663b43443b7
justin.j4ng <2255justin@gmail.com>
2026-09-15 10:40:11 -0700

    Q7 complete

--- Lab02_Section06_JangJustin.r
+++ Lab02_Section06_JangJustin.r
@@ -3,6 +3,9 @@ graphics.off()
 
 setwd("~/pubh_505")
 library(doBy)
+library(glue)
+library(epitools)
+
 
 # 1.
 yale <- read.csv("Yale_Athletics.csv")
@@ -37,20 +40,60 @@ print(height_summaryBK)
 
 # 4. Probability of meeting an athlete > 75
 ht <- yale_subSH$Height
-print(sum(ht > 75) / length(ht))
-# [1] 0.4117647
+prob_tall <- round((sum(ht > 75) / length(ht)) * 100, digits = 2)
+glue("{prob_tall}%")
+# 41.18%
 
 # 5.Relative risk of being tall as a basketball player vs baseball player
 S <- subset(yale_subSH, subset = Sport == "Baseball")
 K <- subset(yale_subSH, subset = Sport == "Basketball")
 
-
-print(sum(ht > 75) / (nrow(S)))
-# [1] 0.7777778
-print(sum(ht > 75) / (nrow(K)))
-# [1] 0.875
+STall <-subset(S, subset = Height > 75) # return df of baseball players > 75
+# head(STall)
+rS<- nrow(STall) / nrow(S) #risk of baseball player being tall
 
 
+KTall <-subset(K, subset = Height > 75)
+rK<- nrow(KTall) / nrow(K) #risk of basketball player being tall
+
+# relative risk
+print(rK/rS)
+# [1] 4.78125
+# 4.78 times more likely to be tall as basketball player than as a baseball player
 
 
+# 6. Odds ratio of being tall as a basketball player vs baseball player
+# Odds ratio = odds of tall as basketball player / odds of tall as baseball player
 
+KShort <- nrow(K) - nrow(KTall) # no. short based on total {sport}ball players - tall {sport}ball players
+SShort <- nrow(S) - nrow(STall)
+
+
+odds_K <- nrow(KTall) / KShort # no. tall basketball players / no. short basketball players
+odds_S <- nrow(STall) / SShort
+
+odds_ratio <- odds_K / odds_S
+
+print(odds_ratio)
+# [1] 13.96429 times the odds of being tall as basketball player than baseball player
+
+
+# 7. contingency table
+# basketball/baseball x short/tall
+
+df <- data.frame(
+  Sport = yale_subSH$Sport,
+  Height = c(yale_subSH$Height > 75)
+  )
+
+my_table <- table(df$Sport, df$Height, dnn = c("Sport","> 75cm vs <= 75cm") )
+  print(my_table)
+  # FALSE TRUE
+  # Baseball      46    8
+  # Basketball    14   34
+  
+oddsratio(my_table)
+# odds ratio with 95% C.I.
+# Sport        estimate    lower    upper
+# Baseball    1.00000       NA       NA
+# Basketball 13.34158 5.227217 37.86765
